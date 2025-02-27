2834fe36279dcfd7d8cacb436fec27f1f97ce03f
h1nigami <timokhinivan1@gmail.com>
2025-02-27 15:44:09 +0300

    main

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from aiogram import Bot
 import logging
 import sys
 import asyncio
-from data import db, config
+from data import config
 
 bot = Bot(token=config.get_token())
 
 async def start():
-    await db.create_table()
     await dp.start_polling(bot)
     print('Бот запущен')
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
